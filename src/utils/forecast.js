f4869a0cf2053fb70a549c8d76675d1c057d4193
sungnga <nga422@gmail.com>
2020-04-26 19:16:07 -0700

    Add humidity and precip to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,8 +9,8 @@ const forecast = (lat, long, callback) => {
                 } else if (body.error) {
                     callback('Unable to find location', undefined)
                 } else {
-                    const {weather_descriptions, temperature, feelslike} = body.current
-                    callback(undefined, `${weather_descriptions[0]}. It is currently ${temperature} degrees out. It feels like ${feelslike} degrees out.`)
+                    const {weather_descriptions, temperature, feelslike, humidity, precip} = body.current
+                    callback(undefined, `${weather_descriptions[0]}. It is currently ${temperature} degrees and it feels like ${feelslike} degrees out. The humidity is ${humidity}% with a ${precip}% chance of rain.`)
                 }
             })
 }
